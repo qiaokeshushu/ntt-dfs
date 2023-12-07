cb57ac446f98bde53eec520cf7fe699ee7131d69
liuwei <liuwei@sundear.com>
2023-12-07 18:14:54 +0800

    fix

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,9 +3,10 @@
     <top />
     <navbar />
     <Breadcrumb v-if="path !== '/home'" />
-    <div class="content">
+    <div class="content" :class="{ home_container: path === '/home' }">
       <router-view></router-view>
     </div>
+    <div class="footer">Copyright 2021 ORGANON.All rights reseved</div>
   </div>
 </template>
 
@@ -28,6 +29,17 @@ onMounted(() => {
   min-width: 1280px;
   .content {
     padding: 0 20px;
+    min-height: calc(100vh - 182px);
+    &.home_container {
+      min-height: calc(100vh - 130px);
+    }
+  }
+  .footer {
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    color: #999;
   }
 }
 </style>
--- src/layout/top.vue
+++ src/layout/top.vue
@@ -8,6 +8,7 @@
 .logo {
   width: 100%;
   height: 50px;
+  padding: 0 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
